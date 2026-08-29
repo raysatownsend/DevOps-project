259387c33bf54edadbdf68d2ee4d05e5a4a2ce0e
raysatownsend <raysa.townsend@gmail.com>
2026-08-29 17:30:39 -0300

    ajustando identacao

--- infra/main.tf
+++ infra/main.tf
@@ -35,8 +35,8 @@ module "vpc" {
   name = "${var.project_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway   = false
   enable_dns_hostnames = true
