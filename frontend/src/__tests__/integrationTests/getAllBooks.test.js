8c8af850f1772e78fe63f14e260358378ae47792
raysatownsend <raysa.townsend@gmail.com>
2026-08-29 18:28:38 -0300

    ajuste do teste

--- frontend/src/__tests__/integrationTests/getAllBooks.test.js
+++ frontend/src/__tests__/integrationTests/getAllBooks.test.js
@@ -1,7 +1,18 @@
-import {getAllBooks} from '../../getAllBooks';
+import axios from 'axios';
+import { getAllBooks } from '../../getAllBooks';
+
+jest.mock('axios');
 
 test('get all books from the list', async () => {
+  const mockBooks = [
+    { id: 1, title: 'Test Book 1', author: 'Author 1', genre: 'Genre 1' },
+    { id: 2, title: 'Test Book 2', author: 'Author 2', genre: 'Genre 2' },
+  ];
+
+  axios.get.mockResolvedValueOnce({ data: mockBooks });
+
   const books = await getAllBooks();
+
   expect(books).toBeDefined();
   expect(Array.isArray(books)).toBe(true);
   expect(books.length).toBeGreaterThan(0);
